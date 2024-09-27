4797e3e84b40a3c12028b38429329b4b8cf539ea
grig-iv <abstractgrig@gmail.com>
2024-09-27 22:44:31 +0300

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,35 @@
 {
-  description = "My dwm flake";
+  description = "Dwm flake";
 
-  outputs = { self }:
-  {
-    overlays.default = final: prev: {
-      dwm = prev.dwm.overrideAttrs (oldAttrs: {
-        src = ./.;
-      });
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              dwm = prev.dwm.overrideAttrs {
+                src = ./.;
+              };
+            })
+          ];
+        };
+      in {
+        packages.dwm = pkgs.dwm;
+        defaultPackage = pkgs.dwm;
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [xorg.libX11 xorg.libXft xorg.libXinerama gcc bear];
+        };
+      }
+    );
 }
