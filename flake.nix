8b0a0d14d419a42c31373b5adfb5efed4746b210
grig-iv <abstractgrig@gmail.com>
2024-09-28 11:40:12 +0300

    add pre-commit-hooks

--- flake.nix
+++ flake.nix
@@ -4,15 +4,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , pre-commit-hooks
+    ,
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
@@ -23,19 +27,33 @@
             })
           ];
         };
-      in {
+        pre-commit = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true;
+          };
+        };
+      in
+      {
         packages.dwm = pkgs.dwm;
-        defaultPackage = pkgs.dwm;
 
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            clang-tools
-            xorg.libX11
-            xorg.libXft
-            xorg.libXinerama
-            gcc
-            bear
-          ];
+        checks = {
+          pre-commit-check = pre-commit;
+        };
+
+        devShells.defualt = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              clang-tools
+              xorg.libX11
+              xorg.libXft
+              xorg.libXinerama
+              gcc
+              bear
+            ]
+            ++ pre-commit.enabledPackages;
+
+          inherit (pre-commit) shellHook;
         };
       }
     );
