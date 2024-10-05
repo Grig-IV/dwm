ac6144649dda175a2c29e20b87ddaaec24f0c7cf
grig-iv <abstractgrig@gmail.com>
2024-10-05 23:07:12 +0300

    rules cleanup

--- config.h
+++ config.h
@@ -29,7 +29,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
